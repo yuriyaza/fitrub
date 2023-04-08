2d846a298cefac95e190e97b91339345a948a553
yuriy-aza <yuriy.aza@gmail.com>
2023-04-08 15:01:03 +0300

    Client desire

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,41 +1,41 @@
-(() => {
-  const mobileMenuContainer = document.querySelector('[data-menu-container]');
-  const mobileMenu = document.querySelector('[data-menu]');
-  const openMenuButton = document.querySelector('[data-menu-open]');
-  // const closeMenuButton = document.querySelector('[data-menu-close');
+const mobileMenuContainer = document.querySelector('[data-menu-container]');
+const mobileMenu = document.querySelector('[data-menu]');
+const openMenuButton = document.querySelector('[data-menu-open]');
+// const closeMenuButton = document.querySelector('[data-menu-close');
 
-  mobileMenu.addEventListener('click', clickOnMenu);
-  openMenuButton.addEventListener('click', toggleMenu);
-  // closeMenuButton.addEventListener('click', toggleMenu);
+mobileMenu.addEventListener('click', clickOnMenu);
+openMenuButton.addEventListener('click', toggleMenu);
+// closeMenuButton.addEventListener('click', toggleMenu);
 
-  function toggleMenu() {
-    const isMenuOpen = openMenuButton.getAttribute('aria-expanded') === 'true' || false;
-    openMenuButton.setAttribute('aria-expanded', !isMenuOpen);
+function toggleMenu() {
+  const isMenuOpen = openMenuButton.getAttribute('aria-expanded') === 'true' || false;
+  openMenuButton.setAttribute('aria-expanded', !isMenuOpen);
 
-    mobileMenuContainer.classList.toggle('is-open');
-    mobileMenu.classList.toggle('is-open');
-    openMenuButton.classList.toggle('is-open');
+  mobileMenuContainer.classList.toggle('is-open');
+  mobileMenu.classList.toggle('is-open');
+  openMenuButton.classList.toggle('is-open');
 
-    const scrollLockMethod = !isMenuOpen ? 'disableBodyScroll' : 'enableBodyScroll';
-    bodyScrollLock[scrollLockMethod](document.body);
+  const scrollLockMethod = !isMenuOpen ? 'disablePageScroll' : 'enablePageScroll';
+  scrollLock[scrollLockMethod](document.body);
+}
+
+// Close mobile menu on clicking at the link
+function clickOnMenu(event) {
+  if (event.target.nodeName === 'A') {
+    toggleMenu();
   }
+}
 
-  // Close mobile menu on clicking at the link
-  function clickOnMenu(event) {
-    if (event.target.nodeName === 'A') {
-      toggleMenu();
-    }
-  }
+// Close mobile menu on wider screens if device orientation changes
+window.matchMedia('(min-width: 768px)').addEventListener('change', onChangeOrientation);
 
-  // Close mobile menu on wider screens if device orientation changes
-  window.matchMedia('(min-width: 768px)').addEventListener('change', (event) => {
-    if (!event.matches) return;
+function onChangeOrientation(event) {
+  if (!event.matches) return;
 
-    mobileMenuContainer.classList.remove('is-open');
-    mobileMenu.classList.remove('is-open');
-    openMenuButton.classList.remove('is-open');
+  mobileMenuContainer.classList.remove('is-open');
+  mobileMenu.classList.remove('is-open');
+  openMenuButton.classList.remove('is-open');
 
-    openMenuButton.setAttribute('aria-expanded', false);
-    bodyScrollLock.enableBodyScroll(document.body);
-  });
-})();
+  openMenuButton.setAttribute('aria-expanded', false);
+  scrollLock.enablePageScroll(document.body);
+}
--- js/modal-window.js
+++ js/modal-window.js
@@ -1,64 +1,36 @@
-(() => {
-  const htmlEl = document.querySelector('html');
-  const bodyEl = document.querySelector('body');
-  const backdropEl = document.querySelector('.backdrop');
-  const modalWindowEl = document.querySelector('[data-modal]');
-  const sourceElementEl = document.querySelectorAll('[data-modal-open]');
-  const closeButtonEl = document.querySelector('[data-modal-close]');
+const backdropEl = document.querySelector('.backdrop');
+const modalWindowEl = document.querySelector('[data-modal]');
+const sourceElementEl = document.querySelector('[data-modal-open]');
+const closeButtonEl = document.querySelector('[data-modal-close]');
 
-  sourceElementEl.forEach((element) => {
-    element.addEventListener('click', openModal);
-  });
-  closeButtonEl.addEventListener('click', closeModal);
+sourceElementEl.addEventListener('click', openModal);
+closeButtonEl.addEventListener('click', closeModal);
 
-  function openModal(event) {
-    event.preventDefault();
-    disableScroll();
-    modalWindowEl.classList.add('is-visible');
-    window.addEventListener('keydown', onKeyPress);
-    backdropEl.addEventListener('mousedown', onBackdropClick);
+function openModal(event) {
+  if (event.target.nodeName !== 'IMG') {
+    return;
   }
+  event.preventDefault();
+  scrollLock.disablePageScroll(document.body);
+  modalWindowEl.classList.add('is-visible');
+  window.addEventListener('keydown', onKeyPress);
+  backdropEl.addEventListener('mousedown', onBackdropClick);
+}
 
-  function closeModal() {
-    window.removeEventListener('keydown', onKeyPress);
-    modalWindowEl.classList.remove('is-visible');
-    enableScroll();
+function closeModal() {
+  scrollLock.enablePageScroll(document.body);
+  modalWindowEl.classList.remove('is-visible');
+  window.removeEventListener('keydown', onKeyPress);
+}
+
+function onKeyPress(event) {
+  if (event.code === 'Escape') {
+    closeModal();
   }
+}
 
-  function onKeyPress(event) {
-    if (event.code === 'Escape') {
-      closeModal();
-    }
+function onBackdropClick(event) {
+  if (event.target === event.currentTarget) {
+    closeModal();
   }
-
-  function onBackdropClick(event) {
-    if (event.target === event.currentTarget) {
-      closeModal();
-    }
-  }
-
-  function disableScroll() {
-    let pagePosition = window.scrollY;
-    let pageWidth = bodyEl.clientWidth;
-
-    bodyEl.classList.add('disable-scroll');
-    bodyEl.dataset.position = pagePosition;
-    bodyEl.style.top = `${-pagePosition}px`;
-
-    pageWidth = bodyEl.clientWidth - pageWidth;
-    bodyEl.style.paddingRight = `${pageWidth}px`;
-    htmlEl.style.scrollBehavior = 'auto';
-  }
-
-  function enableScroll() {
-    let pagePosition = parseInt(bodyEl.dataset.position, 10);
-
-    bodyEl.style.top = 'auto';
-    bodyEl.classList.remove('disable-scroll');
-    window.scroll({top: pagePosition, left: 0});
-    bodyEl.removeAttribute('data-position');
-
-    bodyEl.style.paddingRight = '';
-    htmlEl.style.scrollBehavior = 'smooth';
-  }
-})();
+}
